9b02a7e76c368310fbd5624dba0807021346af6c
raunakroyy04-coder <raunakroyy04@gmail.com>
2026-08-26 16:38:12 +0530

    Time: 3 ms (41.11%) | Memory: 46.5 MB (47.06%) - LeetSync

---
+++ 445-add-two-numbers-ii/add-two-numbers-ii.java
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> st1=new Stack<>();
+        Stack<Integer> st2=new Stack<>();
+
+        while(l1!=null){
+            st1.push(l1.val);
+           l1= l1.next;
+        }
+        while(l2!=null){
+            st2.push(l2.val);
+            l2=l2.next;
+        }
+        ListNode head=null;
+        int carry=0;
+        while(!st1.isEmpty()||!st2.isEmpty()||carry!=0){
+            int sum=carry;
+            if(!st1.isEmpty()){
+                sum+=st1.pop();
+            }
+            if(!st2.isEmpty()){
+                sum+=st2.pop();
+            }
+            carry=sum/10;
+            ListNode node=new ListNode(sum%10);
+            node.next=head;
+            head=node; 
+        }
+        return head;
+
+    }
+}
