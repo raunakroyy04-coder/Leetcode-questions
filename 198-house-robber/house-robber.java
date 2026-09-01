4942f1e908beb2cd2d4f60f9227110a40bb4876b
raunakroyy04-coder <raunakroyy04@gmail.com>
2026-09-01 19:18:24 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (99.28%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -8,7 +8,7 @@ class Solution {
         return dp[i]=Math.max(take,skip);
     }
     public int rob(int[] nums) {
-        int dp[]=new int[nums.length+1];
+        int dp[]=new int[nums.length];
         Arrays.fill(dp,-1);
         return maxcash(nums,0,dp);
 
